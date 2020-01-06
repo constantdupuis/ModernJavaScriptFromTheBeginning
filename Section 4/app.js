5e45c40e21253b8f0489a0ee484d5de2d420b09a
Constant Dupuis <constant.dupuis@gmail.com>
2020-01-07 00:48:19 +0100

    start adding javascript code

---
+++ Section 4/app.js
@@ -0,0 +1,8 @@
+// Define UI vars
+const form = document.querySelector('#task-form');
+const tasklist = document.querySelector('.collection'); 
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+
+let val = filter;
+console.log(val);
