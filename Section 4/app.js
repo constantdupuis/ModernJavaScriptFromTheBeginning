8a04662b6c710d5cbf802d9e9e8e95483d88fbfb
Constant Dupuis <constant.dupuis@gmail.com>
2020-01-07 01:21:40 +0100

    Start coding add task event

--- Section 4/app.js
+++ Section 4/app.js
@@ -3,6 +3,45 @@ const form = document.querySelector('#task-form');
 const tasklist = document.querySelector('.collection'); 
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
 
-let val = filter;
+// load all event liseners
+loadEventListeners();
+
+// load all event liseners
+function loadEventListeners()
+{
+  form.addEventListener('submit', addTask);
+  
+}
+
+function addTask(e)
+{
+  if( taskInput.value === '')
+  {
+    alert('Add a task');
+  }
+
+  // create li element
+  const li = document.createElement('li');
+  // Add class
+  li.classList.add('collection-item');
+  // create and append text node
+  li.appendChild( document.createTextNode(taskInput.value) );
+  // create new link element
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  // Add icon HTML
+  link.innerHTML = '<i class="material-icons black-text">delete</i>';
+  li.appendChild(link);
+  // append li to ul
+  tasklist.appendChild(li);
+
+  // clear input
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+
+let val = taskInput;
 console.log(val);
